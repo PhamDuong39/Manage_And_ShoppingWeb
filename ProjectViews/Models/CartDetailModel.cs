9dbf89909518044bc3b4d16be57fad1621473d06
vdt2208 <vudinhthinh22082003@gmail.com>
2023-06-14 21:00:53 +0700

    ShowCart

--- ProjectViews/Models/CartDetailModel.cs
+++ ProjectViews/Models/CartDetailModel.cs
@@ -14,6 +14,7 @@ namespace ProjectViews.Models
         public int quantity { get; set; }
         public int ToTalPrice { get; set; }
         public string ImageSource { get; set; }
+        public float Size { get; set; }
 
     }
 }
